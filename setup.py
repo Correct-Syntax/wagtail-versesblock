2c2aa299e08c6f9f49ea146e7430c11a185cb048
Correct Syntax <correctsyntax@yahoo.com>
2020-11-29 20:34:03 -0600

    bump package version & required wagtail version

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 
 setup(
-  name = 'wagtailversesblock',   
-  packages = ['wagtailversesblock'],   
-  version = '1.0.0',
-  license='BSD 3-Clause',   
+  name = 'wagtailversesblock',
+  packages = ['wagtailversesblock'],
+  version = '1.1.0',
+  license='BSD 3-Clause',
   description = 'Bible verse highlighter/formatter streamfield block for Wagtail CMS.',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
-  author = 'Noah Rahm, Correct Syntax', 
-  author_email = 'correctsyntax@yahoo.com',     
-  url = 'https://github.com/Correct-Syntax/wagtail-versesblock', 
-  keywords = ['wagtail', 'Bible', 'verses', 'highlighter', 'streamfield', 'django'], 
-  install_requires=[           
-    'wagtail>=2.9'
+  author = 'Noah Rahm, Correct Syntax',
+  author_email = 'correctsyntax@yahoo.com',
+  url = 'https://github.com/Correct-Syntax/wagtail-versesblock',
+  keywords = ['wagtail', 'Bible', 'verses', 'highlighter', 'streamfield', 'django'],
+  install_requires=[
+    'wagtail>=2.11'
   ],
   classifiers=[
     'Environment :: Web Environment',
@@ -24,7 +24,7 @@ setup(
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
     'Operating System :: OS Independent',
-    'License :: OSI Approved :: BSD License', 
+    'License :: OSI Approved :: BSD License',
     'Programming Language :: Python :: 3',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
